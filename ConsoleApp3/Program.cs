49d674d3d5b5a7d9e2e2dfe76d29f6387c4d5fd4
OsmanGurbuz <107862900+OsmanGurbuz@users.noreply.github.com>
2022-06-27 14:45:39 +0300

    c# tuttuğun takım ile ona takımın eşleştirilmesi
    c# tuttuğun takım ile ona takımın eşleştirilmesi

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> takim = new List<string>() { "Fenerbahçe", "Beşiktaş", "Sakaryaspor", "Ankara Gücü", "Gençler Birliği", "Kayseri Spor", "Trabzon Spor", "Samsun Spor", "Göztepe", "Kasımpaşa"};
+            Console.WriteLine("Eşleşmeler için Enter'a basınız.");
+            for (int i = 1; i <= takim.Count(); i++)
+            {
+                Console.WriteLine(takim[0] + "-" + takim[i]+" Eşleşmesi");
+                Console.ReadLine();
+            }
+        }
+    }
+}
